6557b0eb45518b0681801ab872089496bb34f4c8
Tortured tutor at Kumon😢 <hasini.vijayinbasri26@ycdsbk12.ca>
2025-10-17 15:23:10 -0400

    well- this is just too sad-

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Odometrytest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Odometrytest.java
@@ -2,56 +2,84 @@ package org.firstinspires.ftc.teamcode;
 
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
-import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorEx;
 import com.qualcomm.robotcore.hardware.PIDCoefficients;
 
-
-@Autonomous(name="Concept: Change PID", group = "Concept")
+@Autonomous(name = "Odometryyyyyy", group = "Concept")
 public class Odometrytest extends LinearOpMode {
+
     private DcMotorEx frontLeftMotor;
     private DcMotorEx frontRightMotor;
     private DcMotorEx backLeftMotor;
     private DcMotorEx backRightMotor;
 
+    // PID coefficients only P and d...
     public static final double NEW_P = 2.5;
-    public static final double NEW_I = 0.1;
     public static final double NEW_D = 0.2;
 
+    // Target encoder ticks - just a hardcoded dummy
+    public static final int TARGET_TICKS = 2000;
+
+    @Override
     public void runOpMode() {
-        // getting the motors
-        frontLeftMotor = (DcMotorEx)hardwareMap.get(DcMotor.class, "FL");
-        frontRightMotor = (DcMotorEx)hardwareMap.get(DcMotor.class, "FR");
-        backLeftMotor = (DcMotorEx)hardwareMap.get(DcMotor.class, "BL");
-        backRightMotor = (DcMotorEx)hardwareMap.get(DcMotor.class, "BR");
+        frontLeftMotor = hardwareMap.get(DcMotorEx.class, "FL");
+        frontRightMotor = hardwareMap.get(DcMotorEx.class, "FR");
+        backLeftMotor = hardwareMap.get(DcMotorEx.class, "BL");
+        backRightMotor = hardwareMap.get(DcMotorEx.class, "BR");
 
-        int motorIndex1 = ((DcMotorEx)frontLeftMotor).getPortNumber();
-        int motorIndex2 = ((DcMotorEx)frontRightMotor).getPortNumber();
-        int motorIndex3 = ((DcMotorEx)backLeftMotor).getPortNumber();
-        int motorIndex4 = ((DcMotorEx)backRightMotor).getPortNumber();
+        // Reset encoders - that's what google said idk if it works??
+        frontLeftMotor.setMode(DcMotorEx.RunMode.STOP_AND_RESET_ENCODER);
+        frontRightMotor.setMode(DcMotorEx.RunMode.STOP_AND_RESET_ENCODER);
+        backLeftMotor.setMode(DcMotorEx.RunMode.STOP_AND_RESET_ENCODER);
+        backRightMotor.setMode(DcMotorEx.RunMode.STOP_AND_RESET_ENCODER);
 
+        frontLeftMotor.setMode(DcMotorEx.RunMode.RUN_USING_ENCODER);
+        frontRightMotor.setMode(DcMotorEx.RunMode.RUN_USING_ENCODER);
+        backLeftMotor.setMode(DcMotorEx.RunMode.RUN_USING_ENCODER);
+        backRightMotor.setMode(DcMotorEx.RunMode.RUN_USING_ENCODER);
 
-        // wait for start command.
+        // setting I as 0 cuz aint using it
+        PIDCoefficients pidNew = new PIDCoefficients(NEW_P, 0.0, NEW_D);
+        frontLeftMotor.setPIDCoefficients(DcMotorEx.RunMode.RUN_USING_ENCODER, pidNew);
+        frontRightMotor.setPIDCoefficients(DcMotorEx.RunMode.RUN_USING_ENCODER, pidNew);
+        backLeftMotor.setPIDCoefficients(DcMotorEx.RunMode.RUN_USING_ENCODER, pidNew);
+        backRightMotor.setPIDCoefficients(DcMotorEx.RunMode.RUN_USING_ENCODER, pidNew);
+
         waitForStart();
 
-        PIDCoefficients pidOrig = frontLeftMotor.getPIDCoefficients(DcMotorEx.RunMode.RUN_USING_ENCODER);
+        // Setting da target position
+        frontLeftMotor.setTargetPosition(TARGET_TICKS);
+        frontRightMotor.setTargetPosition(TARGET_TICKS);
+        backLeftMotor.setTargetPosition(TARGET_TICKS);
+        backRightMotor.setTargetPosition(TARGET_TICKS);
 
-        // change coefficients using methods included with DcMotorEx class.
-        //have to check why PID Coefficients is crossing out ??
-        PIDCoefficients pidNew = new PIDCoefficients(NEW_P, NEW_I, NEW_D);
-        frontLeftMotor.setPIDCoefficients(DcMotorEx.RunMode.RUN_USING_ENCODER, pidNew);
+        frontLeftMotor.setMode(DcMotorEx.RunMode.RUN_TO_POSITION);
+        frontRightMotor.setMode(DcMotorEx.RunMode.RUN_TO_POSITION);
+        backLeftMotor.setMode(DcMotorEx.RunMode.RUN_TO_POSITION);
+        backRightMotor.setMode(DcMotorEx.RunMode.RUN_TO_POSITION);
 
-        // re-read coefficients and verify change according to tom.
-        PIDCoefficients pidModified = frontLeftMotor.getPIDCoefficients(DcMotor.RunMode.RUN_USING_ENCODER);
+        // Setting the powers of the motors cuz yes... should I set it to 1 OvO
+        frontLeftMotor.setPower(0.5);
+        frontRightMotor.setPower(0.5);
+        backLeftMotor.setPower(0.5);
+        backRightMotor.setPower(0.5);
 
-        // display info to user at the telemetry
-        while(opModeIsActive()) {
-            telemetry.addData("Runtime", "%.03f", getRuntime());
-            telemetry.addData("P,I,D (orig)", "%.04f, %.04f, %.0f",
-                    pidOrig.p, pidOrig.i, pidOrig.d);
-            telemetry.addData("P,I,D (modified)", "%.04f, %.04f, %.04f",
-                    pidModified.p, pidModified.i, pidModified.d);
+        // telemetry stuff
+        while (opModeIsActive() &&
+                (frontLeftMotor.isBusy() || frontRightMotor.isBusy() ||
+                        backLeftMotor.isBusy() || backRightMotor.isBusy())) {
+
+            telemetry.addData("FL Pos", frontLeftMotor.getCurrentPosition());
+            telemetry.addData("FR Pos", frontRightMotor.getCurrentPosition());
+            telemetry.addData("BL Pos", backLeftMotor.getCurrentPosition());
+            telemetry.addData("BR Pos", backRightMotor.getCurrentPosition());
             telemetry.update();
         }
+
+        // close motors
+        frontLeftMotor.setPower(0);
+        frontRightMotor.setPower(0);
+        backLeftMotor.setPower(0);
+        backRightMotor.setPower(0);
     }
 }
